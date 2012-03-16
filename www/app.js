210ecacec82c03577eb9b0b1afe0b2dd35087a46
Adrian Kosmaczewski <adrian@akosma.com>
2012-03-16 19:57:37 +0100

    Namespaced the application javascript code

--- www/app.js
+++ www/app.js
@@ -1,33 +1,37 @@
-var setStatusMessage = function (message) {
-    var statusLabel = document.getElementById("statusLabel");
-    statusLabel.innerText = message;
-    setTimeout(clearStatusMessage, 3000);
-};
+var Application = function () {
+    return {
+        setStatusMessage: function (message) {
+            var statusLabel = document.getElementById("statusLabel");
+            statusLabel.innerText = message;
+            setTimeout(Application.clearStatusMessage, 3000);
+        },
+        
+        clearStatusMessage: function () {
+            var statusLabel = document.getElementById("statusLabel");
+            statusLabel.innerText = "";
+        },
+        
+        setLocation: function (latitude, longitude) {
+            var latitudeLabel = document.getElementById("latitudeLabel");
+            var longitudeLabel = document.getElementById("longitudeLabel");
+            latitudeLabel.innerText = latitude;
+            longitudeLabel.innerText = longitude;
+            
+            FakePhoneGap.stopGeolocation();
+            
+            var result = [latitude, longitude];
+            return result.join(", ");
+        },
+        
+        displayImage: function (data) {
+            var selectedImage = document.getElementById("selectedImage");
+            var debugDiv = document.getElementById("debugDiv");
+            debugDiv.innerText = data.substring(0, 35) + "...";
+            selectedImage.src = data;
+            Application.setStatusMessage("Image changed!");
+            return data;
+        }
+    };
+}();
 
-var clearStatusMessage = function () {
-    var statusLabel = document.getElementById("statusLabel");
-    statusLabel.innerText = "";
-};
-
-var setLocation = function (latitude, longitude) {
-    var latitudeLabel = document.getElementById("latitudeLabel");
-    var longitudeLabel = document.getElementById("longitudeLabel");
-    latitudeLabel.innerText = latitude;
-    longitudeLabel.innerText = longitude;
-    
-    FakePhoneGap.stopGeolocation();
-    
-    var result = [latitude, longitude];
-    return result.join(", ");
-};
-
-var displayImage = function (data) {
-    var selectedImage = document.getElementById("selectedImage");
-    var debugDiv = document.getElementById("debugDiv");
-    debugDiv.innerText = data.substring(0, 35) + "...";
-    selectedImage.src = data;
-    setStatusMessage("Image changed!");
-    return data;
-};
-
-FakePhoneGap.init(setStatusMessage);
+FakePhoneGap.init(Application.setStatusMessage);
